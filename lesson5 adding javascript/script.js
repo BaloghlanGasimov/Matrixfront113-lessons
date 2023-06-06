1cab433a8d77a2931f940866dbe0753d5fbddda8
Baloghlan <qasimovbaloglan13@gmail.com>
2023-06-06 18:27:58 +0400

    lesson 5

---
+++ lesson5 adding javascript/script.js
@@ -0,0 +1,79 @@
+const diller = ['Azerbaycan','Turk','Ingilis','Rus']
+
+// console.log();
+
+// console.log(diller[2]+diller[0]);
+// console.log(diller.length);
+
+// console.log(diller.push('Fransiz'));
+// console.log(diller);
+// console.log(diller.);
+
+
+
+
+
+
+// function qirmiziReng(){
+//     console.log("bu qirmizi rengdir.");
+// }
+// function goyReng(){
+//     console.log("bu goy rengdir.");
+// }
+// function yasilReng(){
+//     console.log("bu yasil rengdir.");
+// }
+
+// qirmiziReng();
+// goyReng();
+// yasilReng();
+
+
+// function rengGoster(menimrengim){
+//     console.log("menim rengim budur: " + menimrengim)
+// }
+
+
+// rengGoster('yasil');
+
+
+
+// function edediOrtalama(a,b,c){
+//     console.log("Cavab: " +  (a+b+c)/3)
+// }
+
+// edediOrtalama(4,6,8);
+
+function edediOrtalama(a,b,c){
+    return (a+b+c)/3;
+}
+const alinanCavab = edediOrtalama(10,20,30);
+
+
+const uzerineOnGel =(a)=>{
+    return a+10;
+}
+
+console.log(uzerineOnGel(alinanCavab));
+
+// console.log(uzerineOnGel(7));
+
+
+
+
+
+// const alinanCavab = edediOrtalama(8,6,4);
+
+const alinanCavabustegel2 = edediOrtalama(8,6,4) +2;
+
+console.log("Bizim cavabimiz:  " + alinanCavab);
+console.log("Bizim cavabimiz +2 :  " + alinanCavabustegel2);
+
+
+
+
+
+
+
+
+// console.log('salam  ' + 'necesen');
